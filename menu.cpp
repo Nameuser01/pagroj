90c92dffa8ec17cc459d10552dc459e0c7327e36
Nameuser01 <antoine.rimbault@hotmail.com>
2022-05-07 15:56:39 +0200

    Suppression problème 'press_to_continue'

--- menu.cpp
+++ menu.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 void menu();
+void pressEnter();
 int main()
 {
 	// Déclaration des variables
-	string navigation("*");
+	string navigation("*"), foo("foo");
+
 	while(navigation != "0"
 		|| navigation != "1"
 		|| navigation != "2"
@@ -18,15 +20,60 @@ int main()
 		|| navigation != "c"
 		|| navigation != "d")
 	{
+		// Sélection utilisateur
+		menu();
+		cout << "Quel est votre choix ?\n> " ;
+		cin >> navigation ;
+		cout << "\n" << endl;
+
+		// Fermer l'outil de gestion de OUTIL
 		if(navigation == "0")
 		{
 			cout << "Fin d'exécution du programme !" << endl;
 			return 0;
 		}
+		// visualisation des répertoires de OUTIL
+		else if (navigation == "1") 
+		{
+			system("ls -Rl .");
+		}
+		else if (navigation == "2")
+		{
+			continue;
+		}
+		else if (navigation == "3")
+		{
+			continue;
+		}
+		else if (navigation == "4")
+		{
+			continue;
+		}
+		else if (navigation == "a")
+		{
+			continue;
+		}
+		else if (navigation == "b")
+		{
+			continue;
+		}
+		else if (navigation == "c")
+		{
+			continue;
+		}
+		else if (navigation == "d")
+		{
+			continue;
+		}
+		else
+		{
+			cout << "Entrée incorrecte, réessayez !" << endl;
+		}
+		cout << "\nAppuyez sur entrée pour continuer..."; 
+		pressEnter();
+		system("clear");
 	}
-
-	menu();
-	// system("");
+	
 	return 0;
 }
 
@@ -37,9 +84,17 @@ void menu()
 	cout << "1 - visualisation des répertoires de OUTIL" << endl;
 	cout << "2 - visualisation du répertoire de sortie de OUTIL" << endl;
 	cout << "3 - visualisation des fichiers de sortie bruts de OUTIL" << endl;
-	cout << "4 - visualisation des fichiers de sortie triés de OUTIL" << endl;
-	cout << "a + réorganisation du répertoire OUTIL" << endl;
+	cout << "4 - visualisation des fichiers de sortis triés de OUTIL" << endl;
+	cout << "a + réorgaisation du répertoire OUTIL" << endl;
 	cout << "b + analyse statistique d'un fichier (GUI)" << endl;
 	cout << "c + analyse statistique d'un fichier (SHELL)" << endl;
 	cout << "d + exploration des résultats" << endl;
+	cout << "\n" << endl;
+}
+
+void pressEnter()
+{
+	// Un fois 2 est nécessaire pour que cela soit effectif
+	cin.get();
+	cin.get();
 }
